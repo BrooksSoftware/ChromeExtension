cdf1f241ffdbb078ba37d100151aa096d781017c
Azores Czar <serviceacc.mvp@gmail.com>
2022-02-03 13:37:08 +0800

    delete button

--- scripts.js
+++ scripts.js
@@ -12,6 +12,72 @@ console.log("children1"+JSON.stringify(children[1].innerHTML));
 var name = children[0].getElementsByTagName('h2').item(0);
 console.log("name"+name);*/
 
+var getJSON = function(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	xhr.responseType = 'json';
+	xhr.onload = function() {
+	var status = xhr.status;
+	if (status === 200) {
+		callback(null, xhr.response);
+	} else {
+		callback(status, xhr.response);
+	}
+	};
+	xhr.send();
+};
+
+//get current user id
+chrome.runtime.sendMessage({getUser: "cuid"}, function(response) {
+	localStorage.setItem("cuid", response.currentUser);
+});
+var created_by = localStorage.getItem("cuid");
+
+//get webpage products
+var products = [];
+var prodAsin;//current page product asin
+var prodUqId;//current page product ID
+
+var productUrls = document.querySelectorAll('.a-link-normal');
+for (var i=0; i<productUrls.length; i++){
+	var cleanlink = productUrls[i].href;
+	var regex = RegExp("https://www.amazon.com/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})");
+	m = cleanlink.match(regex);
+	if (m) {
+		products.push([m.toString().split(',')[0]]);
+	}
+};
+
+var uniqueUrl = Array.from(new Set(products.map(JSON.stringify))).map(JSON.parse);
+
+function ExtractASIN(url){
+	var ASINreg = new RegExp(/(?:\/)([A-Z0-9]{10})(?:$|\/|\?)/);
+	var cMatch = url.match(ASINreg);
+	if(cMatch == null){
+	return null;
+	}
+	return cMatch[1];
+}
+var asin = ExtractASIN(window.location.href);
+
+var imgg = $.map($('.imgTagWrapper img'), function(el){
+	return $(el).data();
+});
+var listImages = [];
+for(var i = 0; i < imgg.length; i++){
+	listImages.push(imgg[i].oldHires)
+}
+var images = JSON.stringify(listImages)
+  var link = window.location.href;
+
+var lot = $("#ybr option:selected").val();
+var description = $('#feature-bullets').children('ul').children('li').eq(1).children('span').text();
+if($('#feature-bullets').children('ul').children('li').attr('id') === 'replacementPartsFitmentBullet'){
+	description = $('#feature-bullets').children('ul').children('li').eq(1).children('span').text();
+}else{
+	description = $('#feature-bullets').children('ul').children('li').children('span').first().text();
+}
+
 
 add_save_to_ybr_button();
 //product page
@@ -23,8 +89,6 @@ var productTitle = document.getElementById('productTitle');
 price_parent = document.getElementById('corePrice_desktop');
 var price = price_parent.querySelector('.a-offscreen');
 // console.log("a-offscreen123"+price.innerHTML);
-
-
 
 //Show Modal
 function showModal(contentHtml, buttons) {
@@ -78,50 +142,17 @@ function showModal(contentHtml, buttons) {
 //add button 
 
 button_add = document.getElementById('titleSection');
-var button = document.createElement("button");
-button.id = "btnYbr";
-button.innerHTML = "Add to YBR";
-button.addEventListener('click', function() {
-	//get current user id
-	chrome.runtime.sendMessage({getUser: "cuid"}, function(response) {
-		localStorage.setItem("cuid", response.currentUser);
-	});
-
+var addToYbrBtn = document.createElement("button");
+addToYbrBtn.id = "btnYbr";
+addToYbrBtn.innerHTML = "Add to YBR";
+addToYbrBtn.addEventListener('click', function() {
 	$('.imageThumbnail .a-button-inner').click();
-	function ExtractASIN(url){
-		var ASINreg = new RegExp(/(?:\/)([A-Z0-9]{10})(?:$|\/|\?)/);
-		var cMatch = url.match(ASINreg);
-		if(cMatch == null){
-		return null;
-		}
-		return cMatch[1];
-	}
-	var asin = ExtractASIN(window.location.href);
 
     showModal("", [
 		{
 		  label: "Save",
 		  onClick: (modal) => {
-				
-
-			  	var imgg = $.map($('.imgTagWrapper img'), function(el){
-					return $(el).data();
-				});
-				var listImages = [];
-				for(var i = 0; i < imgg.length; i++){
-					listImages.push(imgg[i].oldHires)
-				}
-				var images = JSON.stringify(listImages)
-			  	var link = window.location.href;
-
-				var lot = $("#ybr option:selected").val();
-				var description = $('#feature-bullets').children('ul').children('li').eq(1).children('span').text();
-				if($('#feature-bullets').children('ul').children('li').attr('id') === 'replacementPartsFitmentBullet'){
-					description = $('#feature-bullets').children('ul').children('li').eq(1).children('span').text();
-				}else{
-					description = $('#feature-bullets').children('ul').children('li').children('span').first().text();
-				}
-				var data = '{"Title":"'+productTitle.innerHTML+'", "COGS":"'+price.innerHTML.replace('$','')+'", "Images":'+images+', "description":"'+description+'", "Listing URL":"'+link+'", "asin":"'+asin+'"}';
+				var data = '{"Title":"'+productTitle.innerHTML+'", "COGS":"'+price.innerHTML.replace('$','')+'", "Images":'+images+', "description":"'+description+'", "Listing URL":"'+link+'", "asin":"'+asin+'", "cuid":"'+created_by+'"}';
 				
 				var settings = {
 				  "url": "https://ybr.app/version-test/api/1.1/obj/products_uniques/bulk",
@@ -148,6 +179,7 @@ button.addEventListener('click', function() {
 					}
 					else{ prodList = prod.split(',') }
 					console.log(res.id);
+					prodUqId = res.id;
 					fetch("https://ybr.app/version-test/api/1.1/obj/productlist/"+list+"", {
 					method: "PATCH",
 					body: JSON.stringify({
@@ -159,6 +191,8 @@ button.addEventListener('click', function() {
 					}).then((response) =>{
 						alert('Successfully added to list');
 						$('.modal__close').click();
+						button_add.removeChild(addToYbrBtn);
+						button_add.appendChild(deleteFromYbrBtn);
 					}).catch((err) => {
 						alert('Failed adding to list');
 					});
@@ -174,23 +208,7 @@ button.addEventListener('click', function() {
 		  triggerClose: false
 		}
 	  ]);
-
-	  var getJSON = function(url, callback) {
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url, true);
-			xhr.responseType = 'json';
-			xhr.onload = function() {
-			var status = xhr.status;
-			if (status === 200) {
-				callback(null, xhr.response);
-			} else {
-				callback(status, xhr.response);
-			}
-			};
-			xhr.send();
-		};
 		
-		var created_by = localStorage.getItem("cuid");
 		console.log(created_by);
 		var dynamic_url = 'https://ybr.app/version-test/api/1.1/obj/productlist?constraints=[{"key":"created by","constraint_type":"equals","value":"'+created_by+'"}]';
 		getJSON(dynamic_url,
@@ -224,7 +242,56 @@ button.addEventListener('click', function() {
 	//   console.log("responsejrb"+response);
 	// });
 }, false);
-button_add.appendChild(button);
+//delete from ybr
+var deleteFromYbrBtn = document.createElement("button");
+deleteFromYbrBtn.id = "deleteFloatingBtn";
+deleteFromYbrBtn.innerHTML = "Delete Product";
+
+var getProduct = 'https://ybr.app/version-test/api/1.1/obj/products_uniques?constraints=[{"key":"asin","constraint_type":"equals","value":"'+asin+'"}, {"key":"cuid","constraint_type":"equals","value":"'+created_by+'"}]';
+getJSON(getProduct,
+    function(err, data) {
+		if (err !== null) {
+			alert('Something went wrong: ' + err);
+		} else {
+			let res = data.response.results;
+			for(var i = 0; i < res.length; i++){
+				prodAsin = res[i]["asin"];
+				prodUqId = res[i]["_id"];
+			}
+			
+			if ( prodAsin === asin ) {
+				button_add.appendChild(deleteFromYbrBtn)
+			} else {
+				button_add.appendChild(addToYbrBtn)
+			}
+		}
+		console.log(prodAsin)
+    }
+);
+
+deleteFromYbrBtn.addEventListener('click', function() {
+	console.log(prodUqId);
+
+	const deleteMethod = {
+		method: 'DELETE',
+		headers: {
+		 'Content-type': 'application/json; charset=UTF-8'
+		},
+	}
+
+	fetch("https://ybr.app/version-test/api/1.1/obj/products_uniques/"+prodUqId+"", deleteMethod) 
+	.then(response => response.json())
+	.then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+	.catch(err => console.log(err))
+	
+	button_add.removeChild(deleteFromYbrBtn);
+	alert("Successfully removed from list")
+	button_add.appendChild(addToYbrBtn);
+
+}, false);
+
+
+
 }
 
 
@@ -283,29 +350,6 @@ $("div.s-result-item").each(function() {
   var divId = $(this).find('div').data('asin');
   console.log(divId);
 });*/
-
-//get webpage products
-var products = [];
-
-var productUrls = document.querySelectorAll('.a-link-normal');
-for (var i=0; i<productUrls.length; i++){
-	var cleanlink = productUrls[i].href;
-	var regex = RegExp("https://www.amazon.com/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})");
-	m = cleanlink.match(regex);
-	if (m) {
-		products.push([m.toString().split(',')[0]]);
-	}
-};
-
-var uniqueUrl = Array.from(new Set(products.map(JSON.stringify))).map(JSON.parse);
-
-//delete product floating btn
-var deleteFloatingBtn = document.createElement( 'button' );
-
-document.body.appendChild( deleteFloatingBtn );
-
-deleteFloatingBtn.id = 'deleteFloatingBtn';
-deleteFloatingBtn.innerHTML = "Delete from YBR";
 
 
 //upload product floating btn
