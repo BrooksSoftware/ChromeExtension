98465084014a5af940783fd003ebf77c85934185
Azores Czar <serviceacc.mvp@gmail.com>
2022-02-05 16:02:13 +0800

    delete

--- scripts.js
+++ scripts.js
@@ -295,7 +295,7 @@ addToYbrBtn.addEventListener('click', function() {
 							description: description,
 							"Listing URL":link,
 							asin: asin,
-							cuid: cuid 
+							cuid: messageResponse.currentUser 
 						}),
 						success:function(res){
 							//   console.log(res);
